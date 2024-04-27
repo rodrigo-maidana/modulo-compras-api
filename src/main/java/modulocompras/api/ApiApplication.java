240c0473e40883be14abf25768685664b0f1b2b6
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-04-27 18:52:37 -0400

    -fix Cors (commented)

--- src/main/java/modulocompras/api/ApiApplication.java
+++ src/main/java/modulocompras/api/ApiApplication.java
@@ -2,6 +2,9 @@ package modulocompras.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -9,5 +12,19 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	/*
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200", "https://cors-test.codehappy.dev/") // Lista explícita de orígenes
+																						// permitidos
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}*/
 }
