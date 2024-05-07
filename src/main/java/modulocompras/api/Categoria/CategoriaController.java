534a6836f0719c7773ed43ef564f0f1c5609d483
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-05-07 15:54:51 -0400

    chore: Remove unused import in CategoriaController

--- src/main/java/modulocompras/api/Categoria/CategoriaController.java
+++ src/main/java/modulocompras/api/Categoria/CategoriaController.java
@@ -1,6 +1,5 @@
 package modulocompras.api.Categoria;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
