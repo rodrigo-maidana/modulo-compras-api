c9325dc9678b026566703b339ae10c9783608b1e
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-05-15 15:28:35 -0400

    chore: Refactor ProductoRepository to include findByEliminadoFalse and findByIdAndEliminadoFalse methods

--- src/main/java/modulocompras/api/producto/ProductoRepository.java
+++ src/main/java/modulocompras/api/producto/ProductoRepository.java
@@ -1,10 +1,26 @@
 package modulocompras.api.producto;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-    // Aquí puedes añadir métodos personalizados de consulta si es necesario
 
+    /**
+     * Obtiene una lista de productos que no han sido eliminados.
+     *
+     * @return la lista de productos no eliminados
+     */
+    public List<Producto> findByEliminadoFalse();
+
+    /**
+     * Obtiene un producto por su ID si no ha sido eliminado.
+     *
+     * @param id el ID del producto
+     * @return el producto si existe y no ha sido eliminado, de lo contrario, un Optional vacío
+     */
+    Optional<Producto> findByIdAndEliminadoFalse(Integer id);
 }
