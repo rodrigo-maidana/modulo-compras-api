a8b975da5ec015a95a016909a57967f655b75205
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-05-15 21:59:13 -0400

    chore: Refactor JwtService to generate JWT token with extra claims and expiration time

--- src/main/java/modulocompras/jwt/JwtService.java
+++ src/main/java/modulocompras/jwt/JwtService.java
@@ -1,12 +1,36 @@
 package modulocompras.jwt;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "JDFAPJKKREME202420242024";
+
     public String getToken(UserDetails user) {
+        return getToken(new HashMap<>(), user);
+    }
+
+    private String getToken(Map<String, Object> extraClaims, UserDetails user) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey() {
         return null;
     }
 
