98e003b682c3393bb5dad7bfb6462742f2badede
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-05-16 18:33:26 -0400

    chore: Update CORS configuration to use allowedOriginPatterns instead of allowedOrigins

--- src/main/java/modulocompras/ApiApplication.java
+++ src/main/java/modulocompras/ApiApplication.java
@@ -2,29 +2,11 @@ package modulocompras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@SuppressWarnings("null")
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("181.121.74.202") // add ip 181.121.74.202
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
 	}
 }
