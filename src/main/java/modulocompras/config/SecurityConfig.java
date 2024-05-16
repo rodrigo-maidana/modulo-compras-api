dec8e0b87b45f5844cf5c3f4f46ebe5a93aea44a
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-05-16 11:54:51 -0400

    chore: Add OpenAPI configuration for bearer token authentication

--- src/main/java/modulocompras/config/SecurityConfig.java
+++ src/main/java/modulocompras/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                                                 .disable())
                                 .authorizeHttpRequests(authRequest -> authRequest
                                                 .requestMatchers("/auth/**").permitAll()
-                                                .requestMatchers("/swagger-ui/**").permitAll()
+                                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                                                 .anyRequest().authenticated())
                                 .sessionManagement(sessionManager -> sessionManager
                                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
