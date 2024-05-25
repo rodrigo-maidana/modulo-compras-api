6712bd9dc547674260d0f91b3e92a29257d8dd91
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-05-24 23:59:49 -0400

    refactor Proveedor entity and related classes

--- src/main/java/modulocompras/api/proveedor/Proveedor.java
+++ src/main/java/modulocompras/api/proveedor/Proveedor.java
@@ -1,17 +1,11 @@
 package modulocompras.api.proveedor;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import modulocompras.api.categoria.Categoria;
 
 @Entity
 @Table(name = "PROVEEDORES")
