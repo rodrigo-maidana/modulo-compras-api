85c4b1085afa73a17a96fd28685e5ef512bda92d
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-05-27 21:11:43 -0400

    feat: Update CotizacionDetalleController to handle updating existing cotizacion detalle

--- src/main/java/modulocompras/api/cotizacion/detalle/CotizacionDetalleController.java
+++ src/main/java/modulocompras/api/cotizacion/detalle/CotizacionDetalleController.java
@@ -6,15 +6,26 @@ import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1/cotizacion-detalles") // Endpoint para Pedidos Detalles
+@RequestMapping("api/v1/cotizacion-detalles") // Endpoint para Cotizacion Detalles
 @Tag(name = "Cotizacion Detalles")
 public class CotizacionDetalleController {
 
     @Autowired
-    private CotizacionDetalleService pedidoDetalleService;
+    private CotizacionDetalleService cotizacionDetalleService;
+
+    // Actualizar un cotización detalle existente
+    @PutMapping("/{id}")
+    public ResponseEntity<CotizacionDetalleDTO> updateCotizacionDetalle(@PathVariable Integer id,
+            @RequestBody CotizacionDetalleDTO cotizacionDetalleDTO) {
+        Optional<CotizacionDetalle> updatedCotizacionDetalle = cotizacionDetalleService.updateCotizacionDetalle(id,
+                cotizacionDetalleDTO);
+        if (updatedCotizacionDetalle.isPresent()) {
+            return ResponseEntity.ok(new CotizacionDetalleDTO(updatedCotizacionDetalle.get()));
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
