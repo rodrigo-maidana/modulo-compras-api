bf8e7a874bdfec771d5fbd34459a6271fc199692
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-06-26 00:04:05 -0400

    chore: Update AsientoService to create asiento and asiento detalles for orden de pago

--- src/main/java/modulocompras/api/asiento/AsientoService.java
+++ src/main/java/modulocompras/api/asiento/AsientoService.java
@@ -3,6 +3,11 @@ package modulocompras.api.asiento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import modulocompras.api.asiento.detalle.AsientoDetalle;
+import modulocompras.api.asiento.detalle.AsientoDetalleRepository;
+import modulocompras.api.orden_pago.OrdenPago;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +16,9 @@ public class AsientoService {
 
     @Autowired
     private AsientoRepository asientoRepository;
+
+    @Autowired
+    private AsientoDetalleRepository asientoDetalleRepository;
 
     public AsientoService(AsientoRepository asientoRepository) {
         this.asientoRepository = asientoRepository;
@@ -34,5 +42,35 @@ public class AsientoService {
             asiento.setEliminado(true);
             asientoRepository.save(asiento);
         });
+    }
+
+    public Asiento crearAsientoPorOrdenDePago(OrdenPago ordenPago) {
+        // Crear el asiento principal
+        Asiento asiento = new Asiento();
+        asiento.setFecha(new Date());
+
+        // Crear detalles del asiento
+        AsientoDetalle detalleDebe = new AsientoDetalle();
+        detalleDebe.setAsiento(asiento);
+        detalleDebe.setCuenta("1.1.1.1"); // Efectivo
+        detalleDebe.setDebe(ordenPago.getMontoTotal());
+        detalleDebe.setHaber(0.0);
+
+        // Guardar el detalle
+        asientoDetalleRepository.save(detalleDebe);
+
+        AsientoDetalle detalleHaber = new AsientoDetalle();
+        detalleHaber.setAsiento(asiento);
+        detalleHaber.setCuenta("1.1.1.2"); // Tarjetas
+        detalleHaber.setDebe(0.0);
+        detalleHaber.setHaber(ordenPago.getMontoTotal());
+
+        // Guardar el detalle
+        asientoDetalleRepository.save(detalleHaber);
+
+        // Guardar el asiento y los detalles
+        asientoRepository.save(asiento);
+
+        return asiento;
     }
 }
